961729e2fd8ea0324b9f4f1f00be6dbf00510cb6
martin <jmartinaguirre79@gmail.com>
2022-07-28 17:23:46 -0300

    Revision Tutor v2

--- js/carrito.js
+++ js/carrito.js
@@ -1,4 +1,16 @@
 // ALGORITMO
-// TAREA: Sumar el total en el carrito de compra
+// CONSEGUIR LOS DATOS DEL PRODUCTO Y MANDARLOS AL CARRITO
+function producto(){
+    let producto1 = {
+        //img: ,
+        nombre: document.getElementById("nombre"), // + el talle del producto 
+        precio: document.getElementById("precio"),
+        cantidad: document.getElementById("cantidad"),
+        subTotal: precio*cantidad,
+    }
+    console.log(producto1)
+}
 
-let sumaTotal = 0;
+producto();
+// ALGORITMO
+// TAREA: Sumar el total en el carrito de compra
