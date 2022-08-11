551fa8342b9d7a134ab942893e093063d4640fd0
martin <jmartinaguirre79@gmail.com>
2022-08-11 19:40:03 -0300

    Creamos una base de datos api.json para consumir la info de los productos desde ahi. Hicimos que esos datos se conviertan en cards

--- js/shop.js
+++ js/shop.js
@@ -22,6 +22,194 @@ document.addEventListener("keyup", busca =>{
 
 
 
+
+
+// CREAR FUNCIONALIDAD DE CARDS DINÁMICAS CONSUMIENDO UNA BASE DE DATOS
+// Seleccionamos el section items, donde van a ir las tarjetas, y lo guardamos en una variable
+const items = document.getElementById("items")
+
+// Conseguir el contenido de template(la guia de la card, porque template no se ve en la página)
+const templateCard = document.getElementById("template__card").content
+const fragmento = document.createDocumentFragment()
+
+// Creamos un objeto que guarde los elementos seleccionados por el boton de ver mas, para el carrito
+let paraCarrito = {} 
+
+
+// CONSUMIR  API.JSON
+// se ejecuta cuando el documento es completamente cargado. Cuando todo el HTML esté cargado y parseado, cargar los datos del api.json
+document.addEventListener("DOMContentLoaded", () =>
+    fetchData()
+)
+
+// escucha cuando clickeamos en un elemento de la section de cards
+items.addEventListener("click", (elementoClickeado) => {
+    addCarrito(elementoClickeado)
+})
+
+// Usar fetch para conseguir los datos del archivo api.json
+        // async y await para esperar a que se lea la informacion de la base de datos, y guardarla
+const fetchData = async () => {
+    try {
+        const respuesta = await fetch("../js/api.json")
+        const data = await respuesta.json()
+
+        printCards(data)
+
+    } catch (error) {
+        // si da error, registrarlo en la consola
+        console.log(error)
+    }
+}
+
+// mostramos la informacion
+
+const printCards = (data) => {
+
+    data.forEach(producto => {
+        // Modificamos el elemento html
+        templateCard.querySelector(".card__img__img").setAttribute("src", producto.foto)
+        templateCard.querySelector("h3").textContent = producto.nombre
+        templateCard.querySelector(".card__txt__p").textContent = producto.precio
+        templateCard.querySelector(".card__txt__btn").dataset.id = producto.id
+
+        const clone = templateCard.cloneNode(true)
+        fragmento.appendChild(clone)
+    })
+    items.appendChild(fragmento)
+}
+
+
+const addCarrito = elementoClickeado => {
+    console.log(elementoClickeado.target)
+    // Si el elemento clickeado contiene la clase card__txt__btn
+    if(elementoClickeado.target.classList.contains("card__txt__btn")){
+        elementoClickeado.parentElement
+        console.log(elementoClickeado.parentElement)
+    }
+    elementoClickeado.stopPropagation()
+
+}
+
+const setCarrito = (objeto) => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* CODIGO CACA
+
+
+// FILTRAR CONTENIDO POR BOTÓN DE LIGA
+// serieA
+// premierLeague
+// argentina
+// ligue1
+// laLiga
+// bundesliga
+// internacional
+    
+
+    // Botón para borrar los filtros
+    let botonEliminarFiltro = document.createElement("button")
+    botonEliminarFiltro.innerHTML =  "Eliminar filtros"
+    botonEliminarFiltro.classList.add("eliminarFiltro")
+    document.body.append(botonEliminarFiltro)
+
+
+
+    /*
+document.addEventListener("mousedown", filtrarLiga => {
+    let botonEliminarFiltro = document.createElement("button")
+    botonEliminarFiltro.innerHTML =  <button class="eliminar_filtros"> Eliminar filtros </button>
+    document.body.append(botonEliminarFiltro)
+
+    if(filtrarLiga.target.matches(".serieA")){
+        document.querySelectorAll(".card").forEach(card =>{
+            card.includes(".serieA")
+            // si coincide, le remueve la clase que lo esconde
+            ?card.classList.remove("filtro__sacar")
+            // si no coincide le agrega la clase que lo esconde
+            :card.classList.add("filtro__sacar")
+        })
+    }
+})
+
+
+
+
+
+
 // CREAR ARRAY CON TODAS LAS DIFERENTES CARDS
 
 // Creamos una clase, para que la usen todos los objetos que corresponden a los cards (1 objeto = 1 card)
@@ -34,6 +222,16 @@ class Articulos {
         this.stock = stock      
     }
 }
+
+
+
+
+//POR NOMBRE - RANGO DE PRECIO - SI SON DE DETERMINADA LIGA
+
+
+
+
+
 
 // CREAMOS UN OBJETO POR CADA CARD
 // Para eso primero contamos la cantidad de cards
@@ -49,32 +247,41 @@ console.log(cardsContadas)
 const arrayCards = []
 // Hacemos una funcion que tome los datos de cada card, y los coloque en un objeto nuevo, siguiendo la clase.
 
-    document.querySelectorAll(".card").forEach( () => {} ) 
+    
+
+
+
+
+const nombreArticulo = (document.querySelector(".card__txt__h3").innerHTML)
+console.log(nombreArticulo)*/
 
 
 
 
 
+/*
+
+document.querySelectorAll(".card").forEach((element) => {
+
+    element = nuevoArticulo = new Articulos(
+    //seleccionar nombre
+    
+   )
+})
 
 
+// Agarra el nombre de todos los p con el nombre, y crea un objeto por nombre
+    document.querySelectorAll(".card__txt__h3").forEach((element) => {
+    const nuevoArticulo = new Articulos(
+        
+        (element.innerHTML),
+    )
+    arrayCards.push(nuevoArticulo)})
+
+console.log(arrayCards)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 
@@ -88,7 +295,7 @@ for(let i= 0 ; i< arrayCards.length ; i++){
 */
 
 /*
-   const fotoArticulo = (document.querySelector(".card__img").innerHTML)
+    const fotoArticulo = (document.querySelector(".card__img").innerHTML)
     console.log(fotoArticulo)
 
     const nombreArticulo = (document.querySelector(".card__txt__h3").innerHTML)
@@ -109,4 +316,4 @@ for(let i= 0 ; i< arrayCards.length ; i++){
      } )
     
 */
-//POR NOMBRE - RANGO DE PRECIO - SI SON DE DETERMINADA LIGA
+
